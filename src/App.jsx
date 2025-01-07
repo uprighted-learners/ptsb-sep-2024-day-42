a0147ed0ca88e6a0023a232f79c9a84f0f8d179b
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-01-06 18:24:47 -0600

    rendering dynamic list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
+    const [tasks, setTasks] = useState(["clean room", "wash dishes", "feed cat"])
+
     return (
-        <div>HELLO FROM App.jsx</div>
+        <div>
+            <h1>TASK LIST:</h1>
+            <ul>
+                {tasks.map((task, index) =>
+                    <li key={index}>{task}</li>
+                )}
+            </ul>
+        </div>
     )
 }
